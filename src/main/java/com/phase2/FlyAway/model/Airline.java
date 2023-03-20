d89f911f18331871c2bf2675ed8b71d01c5e93e8
Jamsine <jasmine.r.davies@gmail.com>
2023-03-20 17:04:02 +0000

    created model

--- src/main/java/com/phase2/FlyAway/model/Airline.java
+++ src/main/java/com/phase2/FlyAway/model/Airline.java
@@ -1,8 +1,45 @@
 package com.phase2.FlyAway.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Airline {
+	@Id
+	 
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+	
+	@Column(name="airline_name")
+    public String airlineName;
+	
+	public Airline() {
+		
+	}
+	public Airline(int id, String airlineName) {
+			this.id = id;
+			this.airlineName = airlineName;
+	}
+	
 
+    public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAirlineName() {
+		return airlineName;
+	}
+
+	public void setAirlineName(String airlineName) {
+		this.airlineName = airlineName;
+	}
+
+	
 }
