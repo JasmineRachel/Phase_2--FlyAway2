0e0b64651d8654a1fd6a2acfd390a860310642ee
Jamsine <jasmine.r.davies@gmail.com>
2023-03-20 16:52:32 +0000

    first commit

---
+++ src/test/java/com/phase2/FlyAway/FlyAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phase2.FlyAway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyAwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
