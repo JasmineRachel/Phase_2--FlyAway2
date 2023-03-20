9a89ab1d0671a0300832b82242d2cf00de63a8c9
Jamsine <jasmine.r.davies@gmail.com>
2023-03-20 17:09:40 +0000

    created controller

--- src/main/java/com/phase2/FlyAway/controller/AirlineController.java
+++ src/main/java/com/phase2/FlyAway/controller/AirlineController.java
@@ -1,8 +1,24 @@
 package com.phase2.FlyAway.controller;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.phase2.FlyAway.model.Airline;
+import com.phase2.FlyAway.service.AirlineService;
 
 @RestController
 public class AirlineController {
+	
+	@Autowired
+	AirlineService airlineService;
+	
+	
+	@GetMapping("/airline")
+	public List<Airline> getAirlines(){
+		return airlineService.getAirlines();
+	}
 
 }
