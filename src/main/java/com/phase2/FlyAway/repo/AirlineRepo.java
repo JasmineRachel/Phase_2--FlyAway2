d10368563c3b4b463205825f67eef112b14e035f
Jamsine <jasmine.r.davies@gmail.com>
2023-03-20 17:06:10 +0000

    created repository

--- src/main/java/com/phase2/FlyAway/repo/AirlineRepo.java
+++ src/main/java/com/phase2/FlyAway/repo/AirlineRepo.java
@@ -1,8 +1,11 @@
 package com.phase2.FlyAway.repo;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.phase2.FlyAway.model.Airline;
+
 @Repository
-public interface AirlineRepo {
+public interface AirlineRepo extends CrudRepository<Airline, Integer>{
 
 }
