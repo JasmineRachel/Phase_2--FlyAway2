4efe74e6d038f2748b9ce9d94264e098e2101f3c
Jamsine <jasmine.r.davies@gmail.com>
2023-03-20 17:07:58 +0000

    created service

--- src/main/java/com/phase2/FlyAway/service/AirlineService.java
+++ src/main/java/com/phase2/FlyAway/service/AirlineService.java
@@ -1,8 +1,36 @@
 package com.phase2.FlyAway.service;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.phase2.FlyAway.model.Airline;
+import com.phase2.FlyAway.repo.AirlineRepo;
 
 @Service
 public class AirlineService {
 
+	
+	@Autowired
+	AirlineRepo airlineRepo;
+	
+	public Airline getAirlineById(Integer id) {
+		return airlineRepo.findById(id).get();
+	}
+	
+	public List<Airline> getAirlines(){
+		List<Airline> airlines = new ArrayList<Airline>();
+		airlineRepo.findAll().forEach(airline -> airlines.add(airline));
+		return airlines;
+	}
+	
+	public void createAirline(Airline airline) {
+	
+		airlineRepo.save(airline);
+	}
+	
+	public void deleteAirline(Integer id) {
+		airlineRepo.deleteById(id);
+	}
 }
